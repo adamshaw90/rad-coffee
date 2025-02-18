57903514db24d0ff4aec0bec70c1867bbd68858d
adamshaw90 <adamoshaw90@gmail.com>
2025-02-18 18:03:36 +1100

    Add url paths to global url.py file

--- rad/urls.py
+++ rad/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from store.views import home_view, wholesale_view, about_view, contact_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('admin/', admin.site.urls),
+    path('', home_view, name='home'),
+    path('shop/', include('store.urls')),  # Product pages
+    path('wholesale/', wholesale_view, name='wholesale'),
+    path('about/', about_view, name='about'),
+    path('contact/', contact_view, name='contact'),
 ]
